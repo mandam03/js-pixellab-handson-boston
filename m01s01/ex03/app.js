048d79c65ac25175d4d34b2f36ef83aaab241cb1
mandam03 <mmiha_49@yahoo.com>
2022-11-29 02:28:49 +0200

    MO01S01-EX3-6 TEMA

---
+++ m01s01/ex03/app.js
@@ -0,0 +1,23 @@
+var inputLength = document.getElementById('length');
+var inputWidth = document.getElementById('width');
+var inputWidth = document.getElementById('height');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener(
+  'submit',
+  function (event) {
+    event.preventDefault();
+    var length = inputLength.value || 0;
+    var width = inputWidth.value || 0;
+    var height = inputHeight.value || 0;
+    var result = 0;
+
+    result = length * width * height;
+
+    elementResult.innerText = result;
+  },
+  false,
+);
+
+// Aici imi da o eroare in pixeltab  5 din 7//
---
+++ m01s01/ex05/app.js
@@ -0,0 +1,13 @@
+var inputRadius = document.getElementById('radius');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var radius = inputRadius.value || 0;
+  var result = 0;
+
+  result = 7 * (4 / 3) * Math.PI * radius;
+
+  elementResult.innerText = result;
+});
