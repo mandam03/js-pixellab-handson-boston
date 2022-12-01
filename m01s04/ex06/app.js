9f7c88f6334f6e4d16686ea549fb3b27c8e449ec
mandam03 <mmiha_49@yahoo.com>
2022-12-01 21:59:34 +0200

    m01s04 ex: 1-6 Clasa

---
+++ m01s04/ex06/app.js
@@ -0,0 +1,33 @@
+var person = {
+  name: 'Mihaela',
+  surname: 'Mandache',
+  age: 28,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Laryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
