8497facfab04cfb8af3573c4299bde4e013cb176
mandam03 <mmiha_49@yahoo.com>
2022-12-15 21:59:59 +0200

    so05 ex 5-8 15.12.2022 Curs

---
+++ m01s05/ex07/app.js
@@ -0,0 +1,41 @@
+const dateBuilder = [
+  function () {
+    return '2022';
+  },
+
+  () => {
+    return 'Decembrie';
+  },
+
+  function () {
+    return '15';
+  },
+];
+
+dateBuilder[3] = () => {
+  return 'Joi';
+};
+
+console.warn(`
+Folosind arrayul dateBuilder. Afiseaza luna curencta.
+`);
+
+console.log(`Astazi este ${dateBuilder[1]()}.`);
+
+console.warn(`
+Afiseaza propozitia: “Suntem in anul an.”.
+`);
+
+console.log(`Suntem in anul ${dateBuilder[0]()}.`);
+
+console.warn(`
+Folosind arrayul dateBuilder. Afiseaza luna curenta.
+`);
+
+console.log(`Suntem in luna ${dateBuilder[1]()} in anul ${dateBuilder[0]()}.`);
+
+console.warn(`
+Afiseaza propozitia: “Astazi este zz.”.
+`);
+
+console.log(`Astazi este ${dateBuilder[3]()}.`);
